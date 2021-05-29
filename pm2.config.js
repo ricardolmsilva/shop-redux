36b943ff788049c6a4afd9eb17c3b55378b47844
Ricardo <ricardo.silva@codecomputerlove.com>
2021-05-29 23:09:00 +0100

    refactor

--- pm2.config.js
+++ pm2.config.js
@@ -16,6 +16,7 @@ module.exports = {
         PM2_SERVE_PATH: 'build',
         PM2_SERVE_PORT: process.env.CLIENT_PORT,
         PM2_SERVE_SPA: 'true',
+        PM2_SERVE_HOMEPAGE: '/index.html',
       },
     },
   ],
